aa790264295cb1eddefc2fd82606f496733314e3
FeryazHacimuradov <tu201906125@code.edu.az>
2022-03-20 23:12:37 +0400

    AddeeDynamicCardToFront

--- js/main.js
+++ js/main.js
@@ -5,3 +5,33 @@
 // function logger() {
 //     console.log("hello World");
 // }
+
+// class Rectangle {
+//     constructor(height, weight) {
+//         this.height = height;
+//         this.weight = weight;
+//     }
+
+//     calcArea() {
+//         return this.height * this.weight;
+//     }
+// }
+
+// class ColoredRectangleWithText extends Rectangle {
+//     constructor(height, weight, text, bgColor) {
+//         super(height, weight);
+//         this.text = text;
+//         this.bgColor = bgColor;
+//     }
+
+//     showMyProps() {
+//         console.log(`Текст: ${this.text}, Цвет: ${this.bgColor}`);
+//     }
+// }
+
+// const div = new ColoredRectangleWithText(5, 6, 'Hello', 'Red');
+// div.showMyProps();
+// console.log(div.calcArea());
+
+// const myArea = new Rectangle(5, 6);
+// console.log(myArea.calcArea());
