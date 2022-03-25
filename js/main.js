9b2108686e7fc35c815863d932dab1b7d7196996
FeryazHacimuradov <tu201906125@code.edu.az>
2022-03-25 15:40:29 +0400

    added npm package and DB

--- js/main.js
+++ js/main.js
@@ -103,3 +103,56 @@
 // Promise.race([test(1000), test(2000)]).then(() => {
 //     console.log('All');
 // });
+
+// Filter //
+
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Vlodemart'];
+
+// const shortName = [];
+// names.filter((name) => {
+//     if (name.length < 4) {
+//         shortName.push(name);
+//     }
+// });
+
+// console.log(shortName);
+
+// Map //
+
+// const answer = ['IVAn', 'aNN', 'KseNIa'];
+
+// const result = answer.map(item => item.toLowerCase());
+// console.log(result);
+
+// Every / Some - return boolean
+
+// const some = [4, 'Ivan', 'Ksenia'];
+
+// console.log(some.some(item => typeof(item) === 'number'));
+// console.log(some.every(item => typeof(item) === 'number'));
+
+// Reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+
+// const res = arr.reduce((sum, current) => sum + current);
+// console.log(res);
+
+
+// const arr = ['apple', 'pear', 'plum'];
+
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+// const obj = {
+//     ivan: 'persone',
+//     ann: 'persone',
+//     dog: 'animal',
+//     cat: 'animal'
+// };
+
+// const newArr = Object.entries(obj)
+//     .filter(item => item[1] === 'persone')
+//     .map(item => item[0]);
+
+// console.log(newArr);
